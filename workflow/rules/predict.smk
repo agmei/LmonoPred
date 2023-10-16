fec86e06845e21a5213c59a9ad7d8994b39121a4
algm3 <algm@food.dtu.dk>
2023-10-16 21:04:13 +0200

    Implementation of disinfectant tolerance prediction rule

--- workflow/rules/predict.smk
+++ workflow/rules/predict.smk
@@ -7,4 +7,15 @@ rule predict_virulence:
     params:
         outd=outd
     shell:
-        "python3 workflow/scripts/virulence_prediction.py --vir-align {input} -outd {params.outd}"
+        "python3 workflow/scripts/virulence_prediction.py --vir-align {input} --outd {params.outd}"
+
+
+rule predict_disinftolerance:
+    input:
+        outd+"/disinf_align_out/disinf_align_identities_out.csv"
+    output:
+        outd+"/disinftolerance_prediction_out.csv"
+    params:
+        outd=outd
+    shell:
+        "python3 workflow/scripts/disinftolerance_prediction.py --disinf-align {input} --outd {params.outd}"
---
+++ workflow/scripts/disinftolerance_prediction.py
@@ -0,0 +1,47 @@
+import os
+import argparse
+import joblib
+import pandas as pd
+import numpy as np
+from sklearn.metrics import f1_score, matthews_corrcoef
+
+
+parser=argparse.ArgumentParser(description="Get's aruments for the script")
+parser.add_argument("--disinf-align", dest="disinf_align", action="store", type=str, default=False,
+                    help="outfile from parsing the gene alignment of the disinfectant prediction genes")
+parser.add_argument("--outd", dest="outd", action="store", type=str, default=os.getcwd(),
+                    help="Path to output directory (rel or abs path)")
+
+argv=parser.parse_args()
+
+disinf_genealign=os.path.realpath(argv.disinf_align)
+outd=os.path.realpath(argv.outd)
+
+
+### disinfectant class prediction ###
+
+# load trained models
+disinf_models_in = os.path.join(os.path.dirname(os.path.abspath(__file__)),f"../../data/trained_models/disinfpred_trained_model.joblib")
+
+best_model_disinf = joblib.load(disinf_models_in)
+
+
+# load the gene alignment file
+disinf_pred_inp=pd.read_csv(disinf_genealign, sep=';', index_col=(0), decimal='.').fillna(0)
+disinf_X_pred=disinf_pred_inp/100
+
+# make prediction 
+disinf_preds=best_model_disinf.predict(disinf_X_pred)
+
+
+# make prediction dataframe and save to csv
+disinf_preds_df=pd.DataFrame(disinf_preds, index=disinf_pred_inp.index, columns=["disinfectant phenotype"])
+disinf_preds_df.to_csv(os.path.join(outd,f"disinftolerance_prediction_out.csv"), sep=";")
+
+
+
+
+
+
+
+    
